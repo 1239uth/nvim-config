b078a59ac23a94e30db1ab3e984f25080fbade6d
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2023-11-03 20:16:59 -0400

    Init commit local

--- init.lua
+++ init.lua
@@ -19,3 +19,8 @@ end
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.g.copilot_assume_mapped = true
